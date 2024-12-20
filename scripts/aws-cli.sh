cca8d54e496ccbb48a574c4af69aeee960323752
Manojvarma Potthuri <137042897+manoj7894@users.noreply.github.com>
2024-12-20 11:55:56 +0530

    Create aws-cli.sh

---
+++ scripts/aws-cli.sh
@@ -0,0 +1,6 @@
+# Install AWS_CLI
+sudo apt-get update -y
+sudo apt-get install -y curl unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
